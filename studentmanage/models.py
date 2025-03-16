433725ea3e6976848f2a1df52d660c5c0289ff20
Kallol7 <kalloldhar7@gmail.com>
2025-03-16 16:03:45 +0600

    fix migrations, update models.py, add fixtures

--- studentmanage/models.py
+++ studentmanage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(
+        max_length=24,
+        error_messages={"incomplete": "Enter a phone number."},
+        validators=[RegexValidator(r"^[0-9]{10,15}$", "Enter a valid phone number.")],
+    )
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
