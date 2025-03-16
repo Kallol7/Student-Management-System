670da9b885fdbef85b01df2c1102c32c2efe6d35
Kallol7 <kalloldhar7@gmail.com>
2025-03-16 13:56:03 +0600

    update base.html, add studentform.html

--- studentmanage/views.py
+++ studentmanage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "studentmanage/index.html")
+
+def student_form(request):
+    return render(request, "studentmanage/studentform.html")
